c4a633dee494303bba3acb4b04caa26f42e4ccc7
Ashton K Ellis <ashtonkellis@gmail.com>
2018-05-21 16:46:56 -0700

    book constructor done, toHtml method done

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,24 +3,33 @@
 var app = app || {};
 
 (function(module) {
-  
-  var book = {};
 
-  book.ENV = {
+  function Book(bookDataObj) {
+    Object.keys(bookDataObj).forEach(key => this[key] = bookDataObj[key]);
+  }
+
+  Book.template = Handlebars.compile($('#book-template').text());
+
+  Book.prototype.toHtml = function () {
+    return Book.template(this);
+  }
+
+  Book.ENV = {
     isProduction: /^(?!localhost|127)/.test(window.location.hostname),
     cloudApiUrl: 'https://ta-booklist.herokuapp.com',
     localApiUrl: 'http://localhost:3000',
     apiUrl: ''
   }
 
-  book.ENV.setAppUrl = function() {
+  Book.ENV.setAppUrl = function() {
     this.apiUrl = this.isProduction ? this.cloudApiUrl : this.localApiUrl;
   }
 
-  book.init = function(){
-    book.ENV.setAppUrl();
-    console.log('hello world', book.ENV);
+  Book.init = function(callback){
+    Book.ENV.setAppUrl();
+    $.get('/books')
+    if (callback) callback();
   }
 
-  module.book = book;
+  module.Book = Book;
 })(app);
